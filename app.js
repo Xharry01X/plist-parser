22fa03102653158d2168d25f44f88d5ecac9a88c
Xharry01X <harshitsingh50621@gmail.com>
2024-10-15 19:15:46 +0530

    completed..

--- app.js
+++ app.js
@@ -4,74 +4,159 @@ const plistContent = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-    <key>WorkflowLibraries</key>
+    <key>WFWorkflowActions</key>
     <array>
         <dict>
-            <key>LibraryName</key>
-            <string>MathOperations</string>
-            <key>Functions</key>
-            <array>
+            <key>WFWorkflowActionIdentifier</key>
+            <string>is.workflow.actions.ask</string>
+            <key>WFWorkflowActionParameters</key>
+            <dict>
+                <key>WFAskActionPrompt</key>
+                <string>Enter first number</string>
+                <key>WFInputType</key>
+                <string>Number</string>
+            </dict>
+        </dict>
+        <dict>
+            <key>WFWorkflowActionIdentifier</key>
+            <string>is.workflow.actions.setvariable</string>
+            <key>WFWorkflowActionParameters</key>
+            <dict>
+                <key>WFVariableName</key>
+                <string>First Number</string>
+            </dict>
+        </dict>
+        <dict>
+            <key>WFWorkflowActionIdentifier</key>
+            <string>is.workflow.actions.ask</string>
+            <key>WFWorkflowActionParameters</key>
+            <dict>
+                <key>WFAskActionPrompt</key>
+                <string>Enter second number</string>
+                <key>WFInputType</key>
+                <string>Number</string>
+            </dict>
+        </dict>
+        <dict>
+            <key>WFWorkflowActionIdentifier</key>
+            <string>is.workflow.actions.setvariable</string>
+            <key>WFWorkflowActionParameters</key>
+            <dict>
+                <key>WFVariableName</key>
+                <string>Second Number</string>
+            </dict>
+        </dict>
+        <dict>
+            <key>WFWorkflowActionIdentifier</key>
+            <string>is.workflow.actions.math</string>
+            <key>WFWorkflowActionParameters</key>
+            <dict>
+                <key>WFMathOperand</key>
                 <dict>
-                    <key>FunctionName</key>
-                    <string>AddNumbers</string>
-                    <key>InputParameters</key>
-                    <array>
-                        <string>Number1</string>
-                        <string>Number2</string>
-                    </array>
-                    <key>OutputParameter</key>
-                    <string>Result</string>
+                    <key>Value</key>
+                    <dict>
+                        <key>Type</key>
+                        <string>Variable</string>
+                        <key>VariableName</key>
+                        <string>Second Number</string>
+                    </dict>
+                    <key>WFSerializationType</key>
+                    <string>WFTextTokenAttachment</string>
                 </dict>
-            </array>
+                <key>WFMathOperation</key>
+                <string>+</string>
+            </dict>
+        </dict>
+        <dict>
+            <key>WFWorkflowActionIdentifier</key>
+            <string>is.workflow.actions.showresult</string>
+            <key>WFWorkflowActionParameters</key>
+            <dict>
+                <key>Text</key>
+                <dict>
+                    <key>Value</key>
+                    <dict>
+                        <key>attachmentsByRange</key>
+                        <dict>
+                            <key>{18, 1}</key>
+                            <dict>
+                                <key>Type</key>
+                                <string>Variable</string>
+                                <key>VariableName</key>
+                                <string>First Number</string>
+                            </dict>
+                            <key>{24, 1}</key>
+                            <dict>
+                                <key>Type</key>
+                                <string>Variable</string>
+                                <key>VariableName</key>
+                                <string>Second Number</string>
+                            </dict>
+                            <key>{30, 1}</key>
+                            <dict>
+                                <key>OutputUUID</key>
+                                <string>2983A2ED-EB78-4F9B-9981-9770EB7B8B79</string>
+                                <key>Type</key>
+                                <string>ActionOutput</string>
+                            </dict>
+                        </dict>
+                        <key>string</key>
+                        <string>The sum of number £ and number £ is £.</string>
+                    </dict>
+                    <key>WFSerializationType</key>
+                    <string>WFTextTokenString</string>
+                </dict>
+            </dict>
         </dict>
     </array>
-    <key>Workflow</key>
+    <key>WFWorkflowIcon</key>
     <dict>
-        <key>Steps</key>
-        <array>
-            <dict>
-                <key>StepName</key>
-                <string>GetUserInput</string>
-                <key>Action</key>
-                <string>PromptForInput</string>
-                <key>Inputs</key>
-                <array>
-                    <string>Enter first number:</string>
-                    <string>Enter second number:</string>
-                </array>
-                <key>Outputs</key>
-                <array>
-                    <string>Number1</string>
-                    <string>Number2</string>
-                </array>
-            </dict>
-            <dict>
-                <key>StepName</key>
-                <string>PerformAddition</string>
-                <key>Action</key>
-                <string>MathOperations.AddNumbers</string>
-                <key>Inputs</key>
-                <array>
-                    <string>Number1</string>
-                    <string>Number2</string>
-                </array>
-                <key>Outputs</key>
-                <array>
-                    <string>Result</string>
-                </array>
-            </dict>
-            <dict>
-                <key>StepName</key>
-                <string>DisplayResult</string>
-                <key>Action</key>
-                <string>ShowOutput</string>
-                <key>Inputs</key>
-                <array>
-                    <string>The sum is: {Result}</string>
-                </array>
-            </dict>
-        </array>
+        <key>WFWorkflowIconStartColor</key>
+        <integer>4282601983</integer>
+        <key>WFWorkflowIconGlyphNumber</key>
+        <integer>59511</integer>
     </dict>
+    <key>WFWorkflowInputContentItemClasses</key>
+    <array>
+        <string>WFAppStoreAppContentItem</string>
+        <string>WFArticleContentItem</string>
+        <string>WFContactContentItem</string>
+        <string>WFDateContentItem</string>
+        <string>WFEmailAddressContentItem</string>
+        <string>WFGenericFileContentItem</string>
+        <string>WFImageContentItem</string>
+        <string>WFiTunesProductContentItem</string>
+        <string>WFLocationContentItem</string>
+        <string>WFDCMapsLinkContentItem</string>
+        <string>WFAVAssetContentItem</string>
+        <string>WFPDFContentItem</string>
+        <string>WFPhoneNumberContentItem</string>
+        <string>WFRichTextContentItem</string>
+        <string>WFSafariWebPageContentItem</string>
+        <string>WFStringContentItem</string>
+        <string>WFURLContentItem</string>
+    </array>
+    <key>WFWorkflowTypes</key>
+    <array>
+        <string>NCWidget</string>
+        <string>WatchKit</string>
+        <string>ActionExtension</string>
+        <string>QuickAction</string>
+    </array>
+    <key>WFWorkflowHasShortcutInputVariables</key>
+    <true/>
+    <key>WFWorkflowMinimumClientVersion</key>
+    <integer>900</integer>
+    <key>WFWorkflowClientVersion</key>
+    <string>1145.11</string>
+    <key>WFWorkflowMinimumClientVersionString</key>
+    <string>900</string>
+    <key>WFWorkflowImportQuestions</key>
+    <array/>
+    <key>WFWorkflowName</key>
+    <string>Add Two Numbers</string>
+    <key>WFWorkflowDescription</key>
+    <string>This shortcut adds two numbers provided by the user.</string>
 </dict>
 </plist>`;
 
